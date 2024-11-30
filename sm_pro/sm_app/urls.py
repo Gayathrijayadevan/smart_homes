3ab2b76286118cf7a1f1cdce4e10d59671f49401
Gayathrijayadevan <gayathrijayadevan11@gmail.com>
2024-11-30 11:31:38 +0530

    smart homes

--- sm_pro/sm_app/urls.py
+++ sm_pro/sm_app/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('login',views.login),
+    path('',views.sm_login),
+    path('logout',views.sm_logout),
+    path('register',views.register),
+    path('user_home',views.user_home),
 ]
--- sm_pro/sm_app/views.py
+++ sm_pro/sm_app/views.py
@@ -1,5 +1,55 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
+from .models import *
+from django.contrib.auth.models import User
 
 # Create your views here.
-def login(req):
-    return render(req,'login.html')
+def sm_login(req):
+        if 'admin' in req.session:
+            return redirect(admin_home)
+        if 'user' in req.session:
+            return redirect(user_home)
+        if req.method=='POST':
+            uname=req.POST['uname']
+            password=req.POST['password']
+            data=authenticate(username=uname,password=password)
+            if data:
+                login(req,data)
+                if data.is_superuser:
+                    req.session['shop']=uname 
+                    return redirect(admin_home)
+                else:
+                    req.session['user']=uname 
+                    return redirect(user_home)
+            else:
+                messages.warning(req,'invalid username or password')
+                return redirect(sm_login)  
+        else:
+            return render(req,'login.html')
+
+def sm_logout(req):
+    logout(req)
+    req.session.flush() 
+    return redirect(sm_login)
+
+def admin_home(req):
+    return render(req,'admin/ad_home.html')
+
+def user_home  (req)  :
+    return render(req,'user/user_home.html')
+
+def register(req):
+    if req.method=='POST':
+        uname=req.POST['uname']
+        email=req.POST['email']
+        pswd=req.POST['pswd']
+        try:
+            data=User.objects.create_user(first_name=uname,email=email,username=email,password=pswd)
+            data.save()
+        except:
+            messages.warning(req,'invalid username or password')
+            return redirect(register)   
+        return redirect(sm_login) 
+    else:
+        return render(req,'user/register.html') 
