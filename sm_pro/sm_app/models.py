2790ad4cb4a27773ae3aa5d57c2127908047b79d
Gayathrijayadevan <gayathrijayadevan11@gmail.com>
2024-12-09 11:33:34 +0530

    admin created

--- sm_pro/sm_app/models.py
+++ sm_pro/sm_app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Product(models.Model):
+    pid=models.TextField()
+    name=models.TextField()
+    des=models.TextField()
+    price=models.IntegerField()
+    offer_price=models.IntegerField()
+    categorie=models.TextField()
+    stock=models.IntegerField()
+    img=models.FileField()
+
+class Cart(models.Model) :
+    product=models.ForeignKey(Product,on_delete=models.CASCADE)
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    qty=models.IntegerField()
+
+class Buy(models.Model) :
+    product=models.ForeignKey(Product,on_delete=models.CASCADE)
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    qty=models.IntegerField()
+    price=models.IntegerField()
+    date=models.DateField(auto_now_add=True)
