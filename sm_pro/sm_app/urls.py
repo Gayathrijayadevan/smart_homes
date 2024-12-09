ac4de583a97a7e530fc124a1a64a0d744a47c573
Gayathrijayadevan <gayathrijayadevan11@gmail.com>
2024-12-10 02:11:41 +0530

    userhome

--- sm_pro/sm_app/urls.py
+++ sm_pro/sm_app/urls.py
@@ -4,6 +4,14 @@ from . import views
 urlpatterns=[
     path('',views.sm_login),
     path('logout',views.sm_logout),
+
+    #------------USER------------------
     path('register',views.register),
     path('user_home',views.user_home),
+    #------------ADMIN-----------------
+    path('admin_home',views.admin_home),
+    path('add_pro',views.add_products),
+    path('edit_pro/<pid>',views.edit_product),
+    path('del_pro/<pid>',views.delete_product),
+
 ]
--- sm_pro/sm_app/views.py
+++ sm_pro/sm_app/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
 from .models import *
 from django.contrib.auth.models import User
+import os
 
 # Create your views here.
 def sm_login(req):
@@ -17,7 +18,7 @@ def sm_login(req):
             if data:
                 login(req,data)
                 if data.is_superuser:
-                    req.session['shop']=uname 
+                    req.session['admin']=uname 
                     return redirect(admin_home)
                 else:
                     req.session['user']=uname 
@@ -32,10 +33,64 @@ def sm_logout(req):
     logout(req)
     req.session.flush() 
     return redirect(sm_login)
-
+#--------------------admin----------------------
 def admin_home(req):
-    return render(req,'admin/ad_home.html')
+    if 'shop' in req.session:
+        data=Product.objects.all()
+        return render(req,'admin/ad_home.html',{'Products':data})
+    else:
+        return redirect(sm_login)
 
+def add_products(req) :
+    if 'shop' in req.session:
+        if req.method=='POST':
+            pid=req.POST['pid']
+            pname=req.POST['name']
+            des=req.POST['descrip']
+            pprice=req.POST['price']
+            oprice=req.POST['off_price']
+            cate=req.POST['Category']
+            pstock=req.POST['stock']
+            file=req.FILES['img']
+
+            data=Product.objects.create(pid=pid,name=pname,des=des,price=pprice,offer_price=oprice,categorie=cate,stock=pstock,img=file)
+            data.save()
+            return redirect(admin_home)
+        else:
+            return render(req,'admin/add_pro.html')
+    else:
+        return redirect(sm_login)    
+    
+def edit_product(req,pid) :
+        if req.method=='POST':
+            proid=req.POST['proid']
+            pname=req.POST['name']
+            des=req.POST['descrip']
+            pprice=req.POST['price']
+            oprice=req.POST['off_price']
+            cate=req.POST['Category']
+            pstock=req.POST['stock']
+            file=req.FILES.get('img')
+            if file:
+                Product.objects.filter(pk=pid).update(pid=proid,name=pname,des=des,price=pprice,offer_price=oprice,categorie=cate,stock=pstock,img=file)
+                data=Product.objects.get(pk=pid)
+                data.img=file
+                data.save()
+            else:  
+                Product.objects.filter(pk=pid).update(pid=pid,name=pname,des=des,price=pprice,offer_price=oprice,stock=pstock,img=file)
+            return redirect(admin_home)
+        else:
+            data=Product.objects.get(pk=pid)
+            return render(req,'admin/edit_pro.html',{'data':data})
+
+def delete_product(req,pid):
+    data=Product.objects.get(pk=pid)
+    file=data.img.url
+    file=file.split('/')[-1]
+    os.remove('media/'+file)
+    data.delete()
+    return redirect(admin_home)    
+#-------------user------------------------------
 def user_home  (req)  :
     return render(req,'user/user_home.html')
 
